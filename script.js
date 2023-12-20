1abc07b0c3cc6539c4ea5462ca2cd2ce936bd9aa
Peter <87454888+Peteryhs11@users.noreply.github.com>
2023-12-20 01:27:34 +0000

    modified:   Static/style.css 	modified:   index.html 	modified:   script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    setTimeout(function() {
-        document.querySelector('.center .qr-code').style.animationPlayState = 'running';
-    }, 2000);
-});
